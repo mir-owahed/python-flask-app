0929b5a4a17b8d003b9657c994bdc5c4b366e324
Mir Ali <bachchu333@gmail.com>
2024-06-13 15:39:13 +0530

    python flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '1st code on Python Flask!'
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
+    
+    
+@app.route('/ping')
+def ping():
+    return 'PONG!'
+    
+    
+app.run(host='0.0.0.0', port=81)
